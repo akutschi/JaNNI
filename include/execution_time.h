ae926492185d856742575c05b3289df4a45a6eaa
Arthur <akutschi@gmail.com>
2020-04-09 14:55:52 -0400

    Refactoring and changed format style
    Renaming member variables, adding trailing to underscore to distinguish
    from other variables.
    
    Applying default format style from VS Code - Microsoft C++ style.

--- include/execution_time.h
+++ include/execution_time.h
@@ -3,15 +3,16 @@
 
 #include <chrono>
 
-
-class ExecutionTime {
+class ExecutionTime
+{
 public:
     void set_start_time();
     void set_end_time();
     void duration_time();
+
 private:
-    std::chrono::high_resolution_clock::time_point t_start;
-    std::chrono::high_resolution_clock::time_point t_end;
+    std::chrono::high_resolution_clock::time_point t_start_;
+    std::chrono::high_resolution_clock::time_point t_end_;
 };
 
 #endif
--- src/execution_time.cpp
+++ src/execution_time.cpp
@@ -4,15 +4,18 @@
 
 #include "execution_time.h"
 
-
-void ExecutionTime::set_start_time() {
-    t_start = std::chrono::system_clock::now();
+void ExecutionTime::set_start_time()
+{
+    t_start_ = std::chrono::system_clock::now();
 }
 
-void ExecutionTime::set_end_time() {
-    t_end = std::chrono::system_clock::now();
+void ExecutionTime::set_end_time()
+{
+    t_end_ = std::chrono::system_clock::now();
 }
 
-void ExecutionTime::duration_time() {
-    std::cout << "Execution time: " << std::chrono::duration_cast<std::chrono::seconds>(t_end - t_start).count() << " seconds\n" << std::endl;
+void ExecutionTime::duration_time()
+{
+    std::cout << "Execution time: " << std::chrono::duration_cast<std::chrono::seconds>(t_end_ - t_start_).count() << " seconds\n"
+              << std::endl;
 }
