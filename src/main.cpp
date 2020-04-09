2815defcc2fc779cf25f2396ceccdaf6ede93a1d
Arthur <akutschi@gmail.com>
2020-04-08 21:14:46 -0400

    Create data sets with run_creator class

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,26 @@
 #include <iostream>
-#include <chrono> // NOTE: required for sleep
-#include <thread> // NOTE: required for sleep
 
 #include "print_info.h"
 #include "execution_time.h"
+#include "run_creator.h"
 
-
-int main() {
+int main()
+{   
     PrintInfo output_messages;
 
     output_messages.welcome_message();
 
-    ExecutionTime timer_training;
-    timer_training.set_start_time();
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    timer_training.set_end_time();
-    timer_training.duration_time();
-
-    ExecutionTime timer_testing;
-    timer_testing.set_start_time();
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    timer_testing.set_end_time();
-    timer_testing.duration_time();
+    output_messages.proceed_confirmation();
     
+    ExecutionTime timer_setup;
+    timer_setup.set_start_time();
+    RunCreator run("../mnist_data/train-images-idx3-ubyte", "../mnist_data/train-labels-idx1-ubyte", // Training data set
+                   "../mnist_data/t10k-images-idx3-ubyte", "../mnist_data/t10k-labels-idx1-ubyte",   // Test data set
+                   {768, 200, 40, 10},                                                               // Layer settings, nodes per layer
+                   0.05);                                                                            // Learning rate
+    timer_setup.set_end_time();
+    timer_setup.duration_time();
+
     output_messages.done_message();
 
     return 0;
