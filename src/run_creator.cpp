b9b8f80c5d0a7ebca279892e7828e5c417e0c373
Arthur <akutschi@gmail.com>
2020-04-09 21:46:08 -0400

    Add member objects
    Constructor calls constructors of member objects and feeds them with
    the required paramters.

--- src/run_creator.cpp
+++ src/run_creator.cpp
@@ -1,26 +1,15 @@
 #include "run_creator.h"
-#include "hyper_parameters.h"
-#include "mnist_data.h"
 
-#include <iostream>
-
+// Constructor calls class constructors of member objects and feeds
+// them with the required parameters
 RunCreator::RunCreator(const std::string &training_data_path,
                        const std::string &training_label_path,
                        const std::string &test_data_path,
                        const std::string &test_label_path,
                        const std::initializer_list<int> &layer_settings,
                        const float &learning_rate)
-{
-}
-
-void RunCreator::setup_training_data_set(const std::string &data_path, const std::string &label_path)
-{
-}
-
-void RunCreator::setup_hyper_parameters(const std::initializer_list<int> &layer_settings, const float &learning_rate)
-{
-}
-
-void RunCreator::setup_test_data_set(const std::string &data_path, const std::string &label_path)
+    : _mnist_training_data(training_data_path, training_label_path),
+      _mnist_test_data(test_data_path, test_label_path),
+      _hyper_parameters(layer_settings, learning_rate)
 {
 }
