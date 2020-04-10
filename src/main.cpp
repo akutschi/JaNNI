3fce6964eb0bdffe81ba8b43c97121a620db87e0
Arthur <akutschi@gmail.com>
2020-04-09 21:47:39 -0400

    Fix wrong function name for timer start and stop

--- src/main.cpp
+++ src/main.cpp
@@ -13,12 +13,12 @@ int main()
     output_messages.proceed_confirmation();
     
     ExecutionTime timer_setup;
-    timer_setup.set_start_time();
+    timer_setup.start_time();
     RunCreator run("../mnist_data/train-images-idx3-ubyte", "../mnist_data/train-labels-idx1-ubyte", // Training data set
                    "../mnist_data/t10k-images-idx3-ubyte", "../mnist_data/t10k-labels-idx1-ubyte",   // Test data set
                    {768, 200, 40, 10},                                                               // Layer settings, nodes per layer
                    0.05);                                                                            // Learning rate
-    timer_setup.set_end_time();
+    timer_setup.end_time();
     timer_setup.duration_time();
 
     output_messages.done_message();
